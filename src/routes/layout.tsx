0f390aef9f81e1e4b44ca27fc363c1c5a536f174
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2023-11-25 19:16:29 +0330

    update

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,7 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-export const onGet: RequestHandler = async ({ cacheControl, headers }) => {
+export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.builder.io/docs/caching/
   cacheControl({

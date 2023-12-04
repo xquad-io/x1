6540d51a286782b430efcea399679266a16b399b
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2023-12-04 14:11:21 +0330

    update

--- src/routes/t/[id]/index.tsx
+++ src/routes/t/[id]/index.tsx
@@ -141,7 +141,7 @@ export default component$(() => {
         <h2 class="text-2xl font-semibold mb-6 text-gray-300">
           You do not need designers for your application
         </h2>
-        {projectInfo.value.isAuthor && projectInfo.value.defined ? (
+        {projectInfo.value.isAuthor || !projectInfo.value.defined ? (
           <form
             class="flex items-center space-x-3 border-b-2 border-gray-800 py-3"
             preventdefault:submit
